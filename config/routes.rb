72ec2e28f88bbb0f3241519e53d200a002ba8cfc
Buyaki <rittahbuyaki@gmail.com>
2022-05-11 20:53:44 +0300

    Add route for handbags

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  namespace :api do
+    namespace :v1 do
+      get '/handbags', to: 'handbags#index'
+      post '/handbags/create', to: 'handbags#create'
+      delete '/handbag/delete', to: 'handbags#destroy'
+    end
+  end
 end
