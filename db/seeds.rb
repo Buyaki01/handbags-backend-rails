c3b59d69e374463c8f2831f7462699d5cab7f769
Buyaki <rittahbuyaki@gmail.com>
2022-05-11 21:30:23 +0300

    Add to the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+image = 'https://www.freepnglogos.com/uploads/women-bag-png/download-women-bag-transparent-png-image-pngimg-5.png'
+handbag1 = Handbag.create!( name: 'Sling Bag', description: 'Originally made in Italy', photo: image, price: 35)
+handbag2 = Handbag.create!(name: 'Travelling Bag', description: 'Originally made in France', photo: image, price: 65)
