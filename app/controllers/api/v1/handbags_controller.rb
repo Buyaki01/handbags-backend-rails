504b5e7ee1c23c29a48d9095a0181dc567eb4491
Buyaki <rittahbuyaki@gmail.com>
2022-05-11 21:04:26 +0300

    Add create action in the handbags controller

--- app/controllers/api/v1/handbags_controller.rb
+++ app/controllers/api/v1/handbags_controller.rb
@@ -7,4 +7,13 @@ class HandbagController < ActionController::Base
       render json: { success: false, errors: 'Opps! Something went wrong' }, status: :unprocessable_entity
     end
   end
+
+  def create
+    @new_handbag = Handbag.new(name: params[:name], description: params[:description], photo: params[:photo], price: params[:price])
+    if @new_handbag.save
+      render json: { success: true, message: 'Handbag created', data: { handbag: @new_handbag } }, status: :created
+    else
+      render json: { success: false, errors: new_handbag.errors }, status: :unprocessable_entity
+    end
+  end
 end
